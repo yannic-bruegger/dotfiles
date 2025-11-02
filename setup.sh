4d409433921ffae1831e0efdfb5accbdee09c98e
Yannic Brügger <yannic@bruegger.dev>
2025-11-02 13:30:04 +0100

    zsh

--- setup.sh
+++ setup.sh
@@ -5,29 +5,30 @@ sudo pacman -Syu
 
 # Setup login manager and greeter
 
-sudo pacman -S --needed greetd greetd-tuigreet
+sudo pacman -S --needed --noconfirm greetd greetd-tuigreet
 sudo systemctl enable greetd.service
 sudo cp ./etc/greetd/config.toml /etc/greetd/config.toml
 
 # Setup base tools
 
-sudo pacman -S --needed git stow yay zsh ghostty zen-browser ttf-cascadia-code-nerd github-cli
+sudo pacman -S --needed --noconfirm git stow yay zsh ghostty zen-browser ttf-cascadia-code-nerd github-cli
 stow zsh
 stow ghostty
+stow zen
 
 # Setup composer
 
-sudo pacman -S --needed hyprland
+sudo pacman -S --needed --noconfirm hyprland
 stow hyprland
 
 
 # Hyprland must have
 
-sudo pacman -S --needed pipewire wireplumber swaync xdg-desktop-portal-hyprland hyprpolkitagent qt5-wayland qt6-wayland
+sudo pacman -S --needed --noconfirm pipewire wireplumber swaync xdg-desktop-portal-hyprland hyprpolkitagent qt5-wayland qt6-wayland
 
 
 # Hyprland nice to have
 
-sudo pacman -S --needed waybar hyprpaper swww hyprpicker yazi ffmpeg 7zip jq poppler fd ripgrep fzf zoxide resvg imagemagick
+sudo pacman -S --needed --noconfirm waybar hyprpaper swww hyprpicker yazi ffmpeg 7zip jq poppler fd ripgrep fzf zoxide resvg imagemagick
 
-yay -S --needed clipse
+yay -S --needed --noconfirm  clipse
